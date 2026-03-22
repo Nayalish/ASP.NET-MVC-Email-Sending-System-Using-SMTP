93a455a2e3a978cdcd850f551111bbd1f7f574b5
Nayalish <nayalishakhter12@gmail.com>
2026-03-22 13:00:58 +0500

    Making test controller for testing purpose

---
+++ SMTP_Email/Controllers/Test2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMTP_Email.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SMTP_Email/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMTP_Email.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
